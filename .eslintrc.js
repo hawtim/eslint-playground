4272d17d9ee206275b9f76cac9b0d4079942208e
kunkka <hawtimzhang@gmail.com>
2021-08-09 23:18:47 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+    sourceType: "module",
+    ecmaVersion: 2015,
+  },
+  plugins: [
+    "simple-import-sort",
+    "sort-keys"
+  ],
+  extends: ["eslint:recommended", "plugin:import/errors"],
+  rules: {
+    // override/add rules settings
+    semi: [2, "always"],
+    "comma-dangle": [2, "always-multiline"],
+    quotes: [2, "single"],
+    "space-before-function-paren": [
+      2,
+      {
+        anonymous: "never",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+    "object-curly-spacing": [
+      2,
+      "always",
+      { arraysInObjects: true, objectsInObjects: false },
+    ],
+    camelcase: 0,
+    "no-console": 0,
+    "import/no-unresolved": 1,
+    "jsx-quotes": [2, "prefer-double"],
+    // Off the rule sort-imports and import/order to avoid conflicting with simple-import-sort.
+    "sort-imports": 0,
+    "import/order": 0,
+    "simple-import-sort/imports": 2,
+    "simple-import-sort/exports": 2,
+    "import/first": 2,
+    "import/newline-after-import": 2,
+    "import/no-duplicates": 2,
+  },
+  overrides: [
+    {
+      files: ["tests/sort-keys/index.js"],
+      rules: {
+        "sort-keys": "warn",
+      },
+    },
+  ],
+};
